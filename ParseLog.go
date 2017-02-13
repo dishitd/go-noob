accbf818bb11d79ae7d951251f3279767b391185
Dishit <dishid@gmail.com>
2017-02-13 11:36:58 +1100

    Updated with averages and SLA calculation
    It also includes parsing a directory of log files rather than only one file

--- ParseLog.go
+++ ParseLog.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 	"regexp"
 	"strconv"
+	"path/filepath"
+	"io/ioutil"
 
 )
 
@@ -22,54 +24,79 @@ func check(e error) {
 
 func main() {
 	//fmt.Printf("hello, world\n")
-
-	file, err := os.Open("e:/FinCrime/epe27.log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	//var responseTimes []int
-	var readLine =""
-	var i int
-	scanner := bufio.NewScanner(file)
-	i=0
+	//Read the list of files from directory
+	var dirPath string = "E:/FinCrime/TestResults/0208_PeakLoad"
+	var filePath string
+	var writeFileName string
 	var total int = 0
 	var max int = 0
 	var countSLA int = 0
-	//responseTimes[0] = 0
-	for scanner.Scan() {
-		readLine = scanner.Text()
-		//fmt.Println(i)
-		if strings.Contains(readLine, "Orchestrator::Time taken for SAS processing"){
-			var responseTime = regexp.MustCompile(`: ([0-9]+)`)
-			fmt.Println(responseTime.FindStringSubmatch(readLine))
-			var response string =   responseTime.FindStringSubmatch(readLine)[1]
-			//var splitString = strings.Split(responseTime.FindStringSubmatch(readLine), " ")
-			fmt.Println(response)
-			var x int
-			x, err = strconv.Atoi(response)
-			total += x
-			i++
-			if x > 100 {
-				countSLA++
-			}
-			if x > max {
-				max = x
-			}
+	var i int
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writeFileName = filepath.Join(dirPath,"result.txt")
+	fileWrite, err := os.Create(writeFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileWrite.Close()
+	for _, file := range files{
+		filePath = filepath.Join(dirPath,file.Name())
+		fmt.Println("Reading file --> " + filePath)
 
+
+		file, err := os.Open(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		//var responseTimes []int
+		var readLine =""
+
+		scanner := bufio.NewScanner(file)
+		i=0
+
+		//responseTimes[0] = 0
+		for scanner.Scan() {
+			readLine = scanner.Text()
+			//fmt.Println(i)
+			if strings.Contains(readLine, "Orchestrator::Time taken for SAS processing"){
+				var responseTime = regexp.MustCompile(`: ([0-9]+)`)
+				//fmt.Println(responseTime.FindStringSubmatch(readLine))
+				var response string =   responseTime.FindStringSubmatch(readLine)[1]
+				//var splitString = strings.Split(responseTime.FindStringSubmatch(readLine), " ")
+				//fmt.Println(response)
+				var x int
+				x, err = strconv.Atoi(response)
+				total += x
+				i++
+				var writeString string = strconv.Itoa(i) + "," + strconv.Itoa(x)
+				fileWrite.WriteString(writeString +"\n")
+
+				//Calculate max value
+				if x > 100 {
+					countSLA++
+				}
+				if x > max {
+					max = x
+				}
+
+			}
+		}
+		fileWrite.Sync()
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
 		}
 	}
+
 	//var total int = 0
 
-
+	fmt.Printf("Total --> %d", total)
+	fmt.Printf("Count --> %d", i);
 	var average float64 = float64(total) / float64(i)
 	fmt.Printf("Average --> %.2f ms\n" , average)
 	fmt.Printf("Max Value is %d\n", max)
 	fmt.Printf("Count of transactions breaching SLA -> %d", countSLA)
-
-
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
 }
