b01703ad4863d57fd8a8e06030df0b5917a427cf
Dishit <dishid@gmail.com>
2017-05-31 14:10:48 +1000

    Parse logs

--- ParseLog.go
+++ ParseLog.go
@@ -8,11 +8,14 @@ import (
 	"os"
 	"log"
 	"strings"
-	"regexp"
+	//	"regexp"
 	"strconv"
 	"path/filepath"
 	"io/ioutil"
+	//"gonum/stats"
 
+
+	"regexp"
 )
 
 func check(e error) {
@@ -25,27 +28,37 @@ func check(e error) {
 func main() {
 	//fmt.Printf("hello, world\n")
 	//Read the list of files from directory
-	var dirPath string = "E:/FinCrime/TestResults/0208_PeakLoad"
+	//var dirPath string = "E:\FinCrime\prodLogs"
+	var dirPath string = "E:/FinCrime/prodLogs"
 	var filePath string
 	var writeFileName string
-	var total int = 0
-	var max int = 0
-	var countSLA int = 0
+	//var total int = 0
+	//var max int = 0
+	//var countSLA int = 0
 	var i int
+	//var roListener float64        // Elapsed time prior to transaction processing, includes MQGet
+	var dataEnricher int = 0        // Elapsed time of ODE internal overhead prior to MEH database fetch
+
+
+	//var countRoListener int = 0
+	var countDataEnricher int = 0
+
+	//var totalCount float64 = 0.0
+	var max int = 0;
+
 	files, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	writeFileName = filepath.Join(dirPath,"result.txt")
+	writeFileName = filepath.Join(dirPath, "RoListener.txt")
 	fileWrite, err := os.Create(writeFileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fileWrite.Close()
-	for _, file := range files{
-		filePath = filepath.Join(dirPath,file.Name())
+	for _, file := range files {
+		filePath = filepath.Join(dirPath, file.Name())
 		fmt.Println("Reading file --> " + filePath)
-
 
 		file, err := os.Open(filePath)
 		if err != nil {
@@ -53,38 +66,41 @@ func main() {
 		}
 		defer file.Close()
 		//var responseTimes []int
-		var readLine =""
+		var readLine = ""
 
 		scanner := bufio.NewScanner(file)
-		i=0
+		i = 0
 
 		//responseTimes[0] = 0
 		for scanner.Scan() {
 			readLine = scanner.Text()
 			//fmt.Println(i)
-			if strings.Contains(readLine, "Orchestrator::Time taken for SAS processing"){
-				var responseTime = regexp.MustCompile(`: ([0-9]+)`)
+			if strings.Contains(readLine, "Time taken by ROlistener to read"){ // Time Taken by data enricher
+				var responseTime = regexp.MustCompile(`:===([0-9]+)`)
 				//fmt.Println(responseTime.FindStringSubmatch(readLine))
 				var response string =   responseTime.FindStringSubmatch(readLine)[1]
 				//var splitString = strings.Split(responseTime.FindStringSubmatch(readLine), " ")
 				//fmt.Println(response)
 				var x int
 				x, err = strconv.Atoi(response)
-				total += x
+				//total += x
 				i++
 				var writeString string = strconv.Itoa(i) + "," + strconv.Itoa(x)
 				fileWrite.WriteString(writeString +"\n")
+				dataEnricher+= x
 
 				//Calculate max value
-				if x > 100 {
-					countSLA++
-				}
+
 				if x > max {
 					max = x
 				}
 
 			}
+
+			countDataEnricher = i
+
 		}
+
 		fileWrite.Sync()
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
@@ -93,10 +109,13 @@ func main() {
 
 	//var total int = 0
 
-	fmt.Printf("Total --> %d", total)
-	fmt.Printf("Count --> %d", i);
-	var average float64 = float64(total) / float64(i)
+	//fmt.Printf("Total --> %d", total)
+	//fmt.Printf("Count --> %d", i);
+	var average float64 = float64(dataEnricher) / float64(countDataEnricher)
 	fmt.Printf("Average --> %.2f ms\n" , average)
 	fmt.Printf("Max Value is %d\n", max)
-	fmt.Printf("Count of transactions breaching SLA -> %d", countSLA)
+	//fmt.Println("Elapsed time prior to transaction processing, includes MQGet -> ", count66)
+
 }
+
+
